05109156fc56eb3826fb74566347909397f6c19d
Livya Morando <95656558+LivyaMorando@users.noreply.github.com>
2022-02-20 16:51:16 -0300

    Add files via upload

---
+++ Ola.py
@@ -0,0 +1,24 @@
+print ('Olá usuário') # o print traz na tela o que eu quero
+
+a = 5 #variaveis 
+b = 3
+
+soma = a + b 
+quadrado = soma**2 #os dois asteriscos significam elevado ao quadrado
+
+##print('a soma é:', soma, 'o quadrado da soma é:', quadrado) #as virgulas separam o que irá ser plotado
+#para trazer o simbolo de igual da mate,atica representar por ==
+
+if soma==8:
+    print('A soma é 8!')
+else:
+    print('Você errou')
+
+##Aprendendo a criar loop
+
+for n in range(5): #cria todos os valores até o 5 - começa do zero
+    print(n)
+
+for n in range(3):
+    print('gostei dessa aula')
+
